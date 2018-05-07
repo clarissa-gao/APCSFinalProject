df44ae9e4498597cb90f929d43625eaa7ab43d40
gaocl <gaoclarissa@gmail.com>
2018-05-07 03:10:15 -0700

    in

--- APCSFinalProject/src/Instructions.java
+++ APCSFinalProject/src/Instructions.java
@@ -9,14 +9,14 @@ public class Instructions extends JPanel implements ActionListener {
 	
 	public Instructions(Main w) {
 		this.w = w;
-		Font startFont = new Font("Serif", Font.ITALIC, 50);
-		JButton button = new JButton("INSTRUCTIONS");
-		button.setBackground(new Color(175, 244, 255)); 
+		Font startFont = new Font("Montserrat", Font.CENTER_BASELINE, 40);
+		JButton button = new JButton("I N S T R U C T I O N S");
+		button.setBackground(new Color(150, 244, 255)); 
 		button.setOpaque(true); 
 		button.setBorderPainted(false);
 		this.setLayout(null);
 		button.setFont(startFont);
-		button.setBounds(400, 300, 200, 75);
+		button.setBounds(250, 400, 500, 75);
 		button.addActionListener(this);
 		add(button);
 	}
