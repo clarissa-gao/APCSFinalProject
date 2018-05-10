6d350cd46defa6665cae3ff32cec4d6c6f4100be
clarissa-gao <38890666+clarissa-gao@users.noreply.github.com>
2018-05-10 11:20:24 -0700

    Update Character.java

--- APCSFinalProject/src/Character.java
+++ APCSFinalProject/src/Character.java
@@ -5,12 +5,27 @@ import java.util.*;
 
 import processing.core.PImage;
 
+/**
+ * 
+ * @author Clarissa Gao
+ * 
+ * Coded 5/10
+ *
+ *Character that user controls that can move forwards and side to side
+ *, it can also interact with different objects on the grid.
+ *
+ */
 public class Character extends Sprite {
 
 	public static final int CHARACTER_WIDTH = 45;
 	public static final int CHARACTER_HEIGHT = 45;
 	public int collectablesCollected;
-
+	/**
+	 * Constructs a character object
+	 * @param img - png image that represents the Character object
+	 * @param x - X coordinate of location of Character
+	 * @param y - Y coordinate of location of Character
+	 */
 	public Character (PImage img, int x, int y) //x and y can be the grid space
 	{
 		super(img, x, y, CHARACTER_WIDTH, CHARACTER_HEIGHT);
