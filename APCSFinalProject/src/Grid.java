36f14ec727333d64a7e9445c3873c0d722cdbde6
karenli02 <38890677+karenli02@users.noreply.github.com>
2018-05-20 23:18:17 -0700

    Update Grid.java

--- APCSFinalProject/src/Grid.java
+++ APCSFinalProject/src/Grid.java
@@ -19,7 +19,7 @@ public class Grid extends PApplet
 {
 	private int [][] grid;
 	
-	// 0 = empty, 2 = destination, -1 = fire, 3 = potion, 4 = shield, 5 = water bucket, -2 = stone, -3 = hole
+	// 0 = empty, 2 = destination, -1 = fire, 3 = potion, 4 = shield, 5 = water bucket, -2 = black hole
 	private int row, column, pgLocX, pgLocY, numObstructions, collectsDrawn, numCollected; //player grid loc
 	private Character player;
 	private float cellWidth, cellHeight, playerX, playerY;
@@ -58,10 +58,9 @@ public class Grid extends PApplet
 		
 		counter = 0;
 
-		//fire
 		for (int i=0; i<20; i++) 
 		{
-			int rx = (int)(Math.random()*25);
+			int rx = (int)(Math.random()*24);
 			int ry = (int)(Math.random()*25);
 			if (grid[rx][ry] == 0) 
 			{
@@ -74,7 +73,6 @@ public class Grid extends PApplet
 			}
 		}
 		
-		//rock
 		for (int i=0; i<10; i++) 
 		{
 			int rx = (int)(Math.random()*25);
@@ -89,24 +87,7 @@ public class Grid extends PApplet
 				i--;
 			}
 		}
-		
-		//hole
-		for (int i=0; i<10; i++) 
-		{
-			int rx = (int)(Math.random()*25);
-			int ry = (int)(Math.random()*25);
-			if (grid[rx][ry] == 0) 
-			{
-				numObstructions++;
-				grid[rx][ry]=-3;
-			}
-			else 
-			{
-				i--;
-			}
-		}
-		
-		
+
 		
 		int x = (int)(Math.random()*25);
 		int y = (int)(Math.random()*25);
@@ -121,11 +102,6 @@ public class Grid extends PApplet
 			}
 		}
 		collectableImages = new ArrayList<PImage>();
-	}
-	
-	public void clear(int x, int y)
-	{
-		grid[x][y] = 0;
 	}
 	
 	public int getRow()
@@ -191,11 +167,6 @@ public class Grid extends PApplet
 		grid[x][y]=2;
 	}
 	
-	public void setLoc(int x, int y, int val)
-	{
-		grid[x][y]=val;
-	}
-	
 	public boolean hasBucket()
 	{
 		return hasBucket;
@@ -257,9 +228,9 @@ public class Grid extends PApplet
 		if(counter%15==0)
 			move++;
 		
-		PImage finalLoc = marker.loadImage("minnie.png");
+		PImage finalLoc = marker.loadImage("treasurechest.png");
 		PImage potion = marker.loadImage("potion.png");
-		PImage shield = marker.loadImage("mshield.gif");
+		PImage shield = marker.loadImage("shield.jpg");
 		PImage bucket = marker.loadImage("waterbucket.png");
 				
 		cellWidth = width/grid.length; //22.8
@@ -277,7 +248,7 @@ public class Grid extends PApplet
 			{
 				marker.noFill();
 				marker.strokeWeight(1);
-				marker.stroke(0);
+				marker.stroke(150);
 				marker.rect(j*cellWidth + x, i*cellHeight + y, cellWidth, cellHeight);
 				if (grid[i][j]==2 && showFinal==true) 
 					marker.image(finalLoc, j*cellWidth + x, i*cellHeight + y, cellWidth, cellHeight);
@@ -310,7 +281,7 @@ public class Grid extends PApplet
 				int yLoc = (int)(Math.random()*25);
 				if (grid[xLoc][yLoc]==0) 
 				{
-					int collectable = (int)(Math.random()*3);
+					int collectable = (int)(Math.random()*2);
 					grid[xLoc][yLoc]=collectable+3;
 					hasBeenDrawn=true;
 				}
